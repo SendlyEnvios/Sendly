e5e8b6053aaf889639bb53cec34b786e3036c8ae
Matheus <sendlyenvios@gmail.com>
2026-09-14 16:23:06 -0300

    adjustes Leves

--- sendly/src/main/resources/static/js/esqueciSenha.js
+++ sendly/src/main/resources/static/js/esqueciSenha.js
@@ -1,19 +1,19 @@
 const form = document.querySelector('form');
 const errorP = document.querySelector('#errorP');
 form.addEventListener('submit', async (e) => {
-    
     e.preventDefault();
 
     try{
-
     const dados = new FormData(form);
-
+    const email = dados.get('email');
+    
     const data = await fetch('https://sendly-production-b679.up.railway.app/cadastroUpdate',{method: 'POST', body: dados});
         
     if(await data.text() === "Gmail inválido"){
         errorP.innerHTML = 'Gmail incorreto. Tente novamente'
         errorP.style.color = 'red'
     }else if(data.ok){
+        sessionStorage.setItem("email", email);
             window.location.href = "./validarToken.html"
     }
     }catch(e){
--- sendly/src/main/resources/static/js/validarToken.js
+++ sendly/src/main/resources/static/js/validarToken.js
@@ -1,5 +1,7 @@
+const email = sessionStorage.getItem("email");
 const form = document.querySelector('form');
 const errorP = document.querySelector('#errorP');
+const reenviarToken = document.querySelector('#a_criarUmaConta');
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -19,4 +21,10 @@ form.addEventListener('submit', async (e) => {
         errorP.innerHTML = 'Ocorreu um erro. Tente novamente'
         errorP.style.color = 'red'
     };
+});
+
+reenviarToken.addEventListener('click', async (e) => {
+    const dados = new FormData();
+    dados.append('email', email);
+    await fetch('https://sendly-production-b679.up.railway.app/cadastroUpdate',{method: 'POST', body: dados});
 });
